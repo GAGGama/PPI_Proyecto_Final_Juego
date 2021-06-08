adfcf07be988cd271a92b14e28c00fadb478fb44
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-08 18:58:20 -0500

    Prueba 2

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(path.join(__dirname,'public')));
 
-const server = app.listen(app.get('port'),()=>{
-    console.log('server on port ',app.get('port'));
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html");
 });
+
+
+//const server = app.listen(app.get('port'),()=>{
+//    console.log('server on port ',app.get('port'));
+//});
 
 
 /*const mysql = require('mysql');
