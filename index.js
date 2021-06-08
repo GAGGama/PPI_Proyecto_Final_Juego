f55c78809e435c623706239fefbb6b82a400c355
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-08 18:24:02 -0500

    Prueba 1

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const server = app.listen(app.get('port'),()=>{
+    console.log('server on port ',app.get('port'));
+});
+
+
+/*const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host:"us-cdbr-east-04.cleardb.com",
+    user:"b94cca5baae069",
+    password:"0b4080e7",
+    database:"heroku_238f010ee41bd1d"
+});
+
+con.connect(function(err){
+    if(err)throw err;
+    console.log("Conected");
+});*/
