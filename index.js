ee53260be9456da1e31981db31cb15a5b1a92328
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-08 19:49:32 -0500

    Prueba 2

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = process.env.PORT;
 
 //app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname,'public')));
+//app.use(express.static(path.join(__dirname,'public')));
 
 app.use(express.static('public'));
 
-app.listen(port, ()=>{
+app.listen(3000 || process.env.PORT, ()=>{
     console.log(`Example app listen`);
 })
 
