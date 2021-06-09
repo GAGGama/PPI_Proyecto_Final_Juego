35b8f4f9a776248857e52a75c64e82d61a02893d
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-08 20:40:19 -0500

    Prueba 2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT;
 
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 
-//app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use(express.static('public'));
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + 'public/index.html'));
+});
 
 app.listen(3000 || process.env.PORT, ()=>{
     console.log(`Example app listen`);
